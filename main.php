d3b1722260a88d85484bccb3c8f86bd772b38c61
shodayuki <yshoda1227@gmail.com>
2021-10-28 18:20:48 +0900

    インスタンスを実装

--- main.php
+++ main.php
@@ -3,3 +3,13 @@
 
   require_once('./classes/Human.php');
   require_once('./classes/Enemy.php');
+
+  // インスタンス化
+  $tiida = new Human();
+  $goblin = new Enemy();
+
+  $tiida->name = "ティーダ";
+  $goblin->name = "ゴブリン";
+
+  echo $tiida->name . "\n";
+  echo $goblin->name . "\n";
