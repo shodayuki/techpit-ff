63c4e14da507b637548137967dc65e2c3776140c
shodayuki <yshoda1227@gmail.com>
2021-11-13 13:52:38 +0900

    攻撃が終わるまでの処理を追加

--- main.php
+++ main.php
@@ -1,15 +1,26 @@
 <?php
-  // echo "処理のはじまりはじまり～！\n\n";
 
-  require_once('./classes/Human.php');
-  require_once('./classes/Enemy.php');
+require_once('./classes/Human.php');
+require_once('./classes/Enemy.php');
 
-  // インスタンス化
-  $tiida = new Human();
-  $goblin = new Enemy();
+// インスタンス化
+$tiida = new Human();
+$goblin = new Enemy();
 
-  $tiida->name = "ティーダ";
-  $goblin->name = "ゴブリン";
+$tiida->name = "ティーダ";
+$goblin->name = "ゴブリン";
 
-  echo $tiida->name . "\n";
-  echo $goblin->name . "\n";
+while ($tiida->hitPoint > 0 && $goblin->hitPoint > 0) {
+  echo $tiida->name . " : " .$tiida->hitPoint . "/" .$tiida::MAX_HITPOINT . "\n";
+  echo $goblin->name . " : " .$goblin->hitPoint . "/" .$goblin::MAX_HITPOINT . "\n";
+  echo "\n";
+
+  $tiida->doAttack($goblin);
+  echo "\n";
+  $goblin->doAttack($tiida);
+  echo "\n";
+}
+
+echo "★★★ 戦闘終了 ★★★\n\n";
+echo $tiida->name . " : " .$tiida->hitPoint . "/" .$tiida::MAX_HITPOINT . "\n";
+echo $goblin->name . " : " .$goblin->hitPoint . "/" .$goblin::MAX_HITPOINT . "\n\n";
