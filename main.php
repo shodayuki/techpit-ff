831f064dbfb839fab1e1e8af64c2dbb4f9055197
shodayuki <yshoda1227@gmail.com>
2021-11-13 13:54:21 +0900

    ターン数を追加

--- main.php
+++ main.php
@@ -10,7 +10,11 @@ $goblin = new Enemy();
 $tiida->name = "ティーダ";
 $goblin->name = "ゴブリン";
 
+$turn = 1;
+
 while ($tiida->hitPoint > 0 && $goblin->hitPoint > 0) {
+  echo "*** $turn ターン目 ***\n\n";
+
   echo $tiida->name . " : " .$tiida->hitPoint . "/" .$tiida::MAX_HITPOINT . "\n";
   echo $goblin->name . " : " .$goblin->hitPoint . "/" .$goblin::MAX_HITPOINT . "\n";
   echo "\n";
@@ -19,6 +23,8 @@ while ($tiida->hitPoint > 0 && $goblin->hitPoint > 0) {
   echo "\n";
   $goblin->doAttack($tiida);
   echo "\n";
+
+  $turn++;
 }
 
 echo "★★★ 戦闘終了 ★★★\n\n";
